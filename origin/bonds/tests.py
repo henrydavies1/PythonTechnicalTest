ecb0c93736529968bbd7a7e88e0395628a8b3fdc
Henry Davies <henrydavies@hotmail.co.uk>
2020-10-29 12:39:33 +0000

    Add get_legal_name() function to views.py

--- origin/bonds/tests.py
+++ origin/bonds/tests.py
@@ -1,7 +1,27 @@
 from rest_framework.test import APISimpleTestCase
+from django.test import TestCase
+
+from .views import get_legal_name
 
 
 class HelloWorld(APISimpleTestCase):
+
     def test_root(self):
+
         resp = self.client.get("/")
         assert resp.status_code == 200
+
+
+class GetLegalNameTestCase(TestCase):
+
+    def test_correct_legal_name_returned(self):
+
+        self.assertEqual(get_legal_name("HWUPKR0MPOU8FGXBT394"), "APPLE INC.")
+        self.assertEqual(get_legal_name("213800JSUFNZLZLCVJ25"), "JOHN LEWIS PLC")
+        self.assertEqual(get_legal_name("549300FL0LHI0TEZ8V48"), "ORACLE SYSTEMS CORPORATION")
+
+    def test_none_returned_if_an_entity_is_not_found_for_the_lei_code(self):
+
+        self.assertEqual(get_legal_name("HWUPKR0MPOU8FGXBT39"), None)
+        self.assertEqual(get_legal_name("HWUPKR0MPOU8FGXBT3944"), None)
+        self.assertEqual(get_legal_name("99999999999999999999"), None)
--- origin/bonds/views.py
+++ origin/bonds/views.py
@@ -1,7 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import requests
 
 
 class HelloWorld(APIView):
+
     def get(self, request):
+
         return Response("Hello World!")
+
+
+def get_legal_name(lei):
+    """Returns the legal name for the given lei code. If the lei code is invalid or an entity cannot be found for the
+       lei code, then None is returned."""
+
+    gleif_response = requests.get("https://leilookup.gleif.org/api/v2/leirecords?lei=" + lei)
+    if gleif_response.status_code == 200:
+        # If given lei code is valid
+
+        gleif_response_json = gleif_response.json()
+        if len(gleif_response_json) != 0:
+            # If an entity is found for the given lei code
+
+            return gleif_response_json[0]["Entity"]["LegalName"]["$"]
+
+    return None
