08f4c8daa2bb38c92336c3977d1ebe3e77c4b64f
Henry Davies <henrydavies@hotmail.co.uk>
2020-10-29 17:29:19 +0000

    Rework get_legal_name(). Implement POST method for /bonds/ endpoint.

--- origin/bonds/tests.py
+++ origin/bonds/tests.py
@@ -1,7 +1,9 @@
-from rest_framework.test import APISimpleTestCase
+from rest_framework.test import APISimpleTestCase, APITestCase
 from django.test import TestCase
+from datetime import datetime
 
-from .views import get_legal_name
+from .views import get_gleif_response
+from .models import Bond
 
 
 class HelloWorld(APISimpleTestCase):
@@ -12,16 +14,88 @@ class HelloWorld(APISimpleTestCase):
         assert resp.status_code == 200
 
 
-class GetLegalNameTestCase(TestCase):
+class GetGleifResponseTest(TestCase):
 
-    def test_correct_legal_name_returned(self):
+    def test_correct_legal_name_found(self):
 
-        self.assertEqual(get_legal_name("HWUPKR0MPOU8FGXBT394"), "APPLE INC.")
-        self.assertEqual(get_legal_name("213800JSUFNZLZLCVJ25"), "JOHN LEWIS PLC")
-        self.assertEqual(get_legal_name("549300FL0LHI0TEZ8V48"), "ORACLE SYSTEMS CORPORATION")
+        # Find legal name for Apple's LEI code
+        gleif_response_apple = get_gleif_response("HWUPKR0MPOU8FGXBT394")
+        gleif_response_apple_json = gleif_response_apple.json()
+        self.assertEqual(gleif_response_apple_json[0]["Entity"]["LegalName"]["$"], "APPLE INC.")
 
-    def test_none_returned_if_an_entity_is_not_found_for_the_lei_code(self):
+        # Find legal name for John Lewis' LEI code
+        gleif_response_john_lewis = get_gleif_response("213800JSUFNZLZLCVJ25")
+        gleif_response_john_lewis_json = gleif_response_john_lewis.json()
+        self.assertEqual(gleif_response_john_lewis_json[0]["Entity"]["LegalName"]["$"], "JOHN LEWIS PLC")
 
-        self.assertEqual(get_legal_name("HWUPKR0MPOU8FGXBT39"), None)
-        self.assertEqual(get_legal_name("HWUPKR0MPOU8FGXBT3944"), None)
-        self.assertEqual(get_legal_name("99999999999999999999"), None)
+        # Find legal name for Oracle's LEI code
+        gleif_response_oracle = get_gleif_response("549300FL0LHI0TEZ8V48")
+        gleif_response_oracle_json = gleif_response_oracle.json()
+        self.assertEqual(gleif_response_oracle_json[0]["Entity"]["LegalName"]["$"], "ORACLE SYSTEMS CORPORATION")
+
+
+class BondsAPITest(APITestCase):
+
+    def test_status_400_returned_when_lei_code_is_invalid(self):
+
+        bond_with_hyphen_in_lei_code = {
+            "isin": "FR0000131104",
+            "size": 100000000,
+            "currency": "EUR",
+            "maturity": "2025-02-28",
+            "lei": "R0MUWSFPU8MPRO8K5P_3"
+        }
+
+        bond_with_lei_code_that_is_too_long = {
+            "isin": "FR0000131104",
+            "size": 100000000,
+            "currency": "EUR",
+            "maturity": "2025-02-28",
+            "lei": "R0MUWSFPU8MPRO8K5P836"
+        }
+
+        response = self.client.post(path="/bonds/", data=bond_with_hyphen_in_lei_code)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data, "LEI code is invalid")
+
+        response = self.client.post(path="/bonds/", data=bond_with_lei_code_that_is_too_long)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data, "LEI code is invalid")
+
+    def test_status_404_returned_when_entity_doesnt_exist_for_lei_code(self):
+
+        bond_with_non_existing_lei_code = {
+            "isin": "FR0000131104",
+            "size": 100000000,
+            "currency": "EUR",
+            "maturity": "2025-02-28",
+            "lei": "99999999999999999999"
+        }
+
+        response = self.client.post(path="/bonds/", data=bond_with_non_existing_lei_code)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data, "Could not find entity for the given LEI code")
+
+    def test_bond_created_successfully_for_lei_code_that_is_valid_and_exists(self):
+
+        bond_with_correct_details = {
+            "isin": "FR0000131104",
+            "size": 100000000,
+            "currency": "EUR",
+            "maturity": "2025-02-28",
+            "lei": "R0MUWSFPU8MPRO8K5P83"
+        }
+
+        response = self.client.post(path="/bonds/", data=bond_with_correct_details)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, "Bond successfully created.")
+
+        created_bond = Bond.objects.all().last()
+        maturity_date = datetime.strptime(bond_with_correct_details["maturity"], "%Y-%m-%d").date()
+
+        self.assertEqual(created_bond.isin, bond_with_correct_details["isin"])
+        self.assertEqual(created_bond.size, bond_with_correct_details["size"])
+        self.assertEqual(created_bond.currency, bond_with_correct_details["currency"])
+        self.assertEqual(created_bond.maturity, maturity_date)
+        self.assertEqual(created_bond.lei, bond_with_correct_details["lei"])
+        self.assertEqual(created_bond.legal_name, "BNP PARIBAS")
--- origin/bonds/views.py
+++ origin/bonds/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 import requests
 
+from .models import Bond
+
 
 class HelloWorld(APIView):
 
@@ -10,18 +12,39 @@ class HelloWorld(APIView):
         return Response("Hello World!")
 
 
-def get_legal_name(lei):
-    """Returns the legal name for the given lei code. If the lei code is invalid or an entity cannot be found for the
-       lei code, then None is returned."""
+class Bonds(APIView):
+    """/bonds/ endpoint"""
 
-    gleif_response = requests.get("https://leilookup.gleif.org/api/v2/leirecords?lei=" + lei)
-    if gleif_response.status_code == 200:
-        # If given lei code is valid
+    def post(self, request):
+        """POST method"""
+
+        lei_code = request.data.get("lei")
+        if len(lei_code) != 20 or not lei_code.isalnum():
+            return Response(status=400, data="LEI code is invalid")
+
+        gleif_response = get_gleif_response(lei_code)
+
+        if gleif_response.status_code != 200:
+            return Response(status=500, data="Error obtaining legal name from GLEIF API")
 
         gleif_response_json = gleif_response.json()
-        if len(gleif_response_json) != 0:
-            # If an entity is found for the given lei code
+        if len(gleif_response_json) == 0:
+            return Response(status=404, data="Could not find entity for the given LEI code")
 
-            return gleif_response_json[0]["Entity"]["LegalName"]["$"]
+        legal_name = gleif_response_json[0]["Entity"]["LegalName"]["$"]
 
-    return None
+        new_bond = Bond(isin=request.data.get("isin"),
+                        size=request.data.get("size"),
+                        currency=request.data.get("currency"),
+                        maturity=request.data.get("maturity"),
+                        lei=request.data.get("lei"),
+                        legal_name=legal_name)
+        new_bond.save()
+
+        return Response("Bond successfully created.")
+
+
+def get_gleif_response(lei_code):
+    """Given a LEI code, returns the response when searching for the LEI code using the GLEIF API"""
+
+    return requests.get("https://leilookup.gleif.org/api/v2/leirecords?lei=" + lei_code)
